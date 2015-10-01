726e31dc26655e8de59073549fa42489c8a7b2ad
colin <colin@brainbits.ca>
2015-09-30 20:02:56 -0700

    clean up package.js

--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 Package.describe({
-  name: 'colin:backbone-events',
+  name: 'colinligertwood:backbone-events',
   version: '1.2.3',
-  summary: 'Backbone.Events: A module that can be mixed in to *any object* in order to provide it with a custom event channel.',
+  summary: 'A module that can be mixed in to *any object* in order to provide it with a custom event channel.',
   git: 'https://github.com/colinligertwood/meteor-backbone-events.git',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.0.0');
+  api.versionsFrom('1.0.3.1');
   api.addFiles('backbone.events.js');
   api.export('Events', ['server','client']);
 });
